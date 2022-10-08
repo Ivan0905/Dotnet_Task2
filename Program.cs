cf9344d562c9b2f9f4cf494e287a508ab7cf0d4a
Ivan <ia.farafonov@yandex.ru>
2022-10-08 16:09:11 +0300

    Решена задача №15

--- Program.cs
+++ Program.cs
@@ -1,35 +1,35 @@
-﻿
-//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
-//что третьей цифры нет.
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным. 
+//Обязательно сделать проверку на ввод чисел меньше 1 и больше 7
+Console.WriteLine("Введите число от 1 до 7: ");
+int num = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Введите число от 1 до 100000: ");
-int Number = int.Parse(Console.ReadLine()!);
-if ((Number<1)||(Number>100000))
+while ((num < 1) || (num > 7))
 {
-    Console.WriteLine("Введите число согласно условию");
+    Console.WriteLine("Вы ввели не корректное число. Повторите ввод: ");
+    num = int.Parse(Console.ReadLine()!);
 }
-else if (Number <= 100)
-    {
-        if (Number < 100)
-        {
-            Console.WriteLine($"В числе {Number} меньше трех цифр");
-        }
-        else
-        {
-            Console.WriteLine($"Третья цифра числа {Number} - '0'");
-        }
-    }
-    else
-    {
-        int Number2 = Number;
-        while (Number2 > 1000)       //12345
-        {
-            Number2 = Number2 / 10;
-            //Console.WriteLine(Number2);
-        }
-        Number2 = (Number2 - 100) % 10;
-        Console.WriteLine($"Третья цифра числа {Number} - '{Number2}' ");
-    }
-
-
-
+switch (num)
+{
+    case 1:
+        Console.WriteLine("Сегодня понедельник, не выходной день ");
+        break;
+    case 2:
+        Console.WriteLine("Сегодня вторник, не выходной день ");
+        break;
+    case 3:
+        Console.WriteLine("Сегодня среда, не выходной день ");
+        break;
+    case 4:
+        Console.WriteLine("Сегодня четверг, не выходной день ");
+        break;
+    case 5:
+        Console.WriteLine("Сегодня пятница, не выходной день ");
+        break;
+    case 6:
+        Console.WriteLine("Сегодня суббота, выходной день ");
+        break;
+    case 7:
+        Console.WriteLine("Сегодня воскресение, выходной день ");
+        break;
+}
